806f5039915430f51151b0258a218575907259e7
Yhondri <yhondri@gmail.com>
2019-04-06 13:18:02 +0200

    Implementamos funciones de la barrera.

--- p3/p3/barrier.c
+++ p3/p3/barrier.c
@@ -30,6 +30,11 @@ int sys_barrier_init(sys_barrier_t *barrier, unsigned int nr_threads)
 	/* Initialize fields in sys_barrier_t
 	     ... To be completed ....
 	*/
+    
+    barrier->max_threads = nr_threads;
+    pthread_mutex_init(&barrier->mutex, NULL);
+    pthread_cond_init(&barrier->cond, NULL);
+    
 	return 0;
 }
 
@@ -39,6 +44,10 @@ int sys_barrier_destroy(sys_barrier_t *barrier)
 	/* Destroy synchronization resources associated with the barrier
 	      ... To be completed ....
 	*/
+    
+    pthread_mutex_destroy(&barrier->mutex);
+    pthread_cond_destroy(&barrier->cond);
+    
 	return 0;
 }
 
@@ -57,6 +66,22 @@ int sys_barrier_wait(sys_barrier_t *barrier)
 
 	    ... To be completed ....
 	*/
+    
+    //Adquirimos el cerrojo
+    pthread_mutext_lock(&barrier->mutex);
+    
+    //bloqueamos el hilo.
+    if (barrier->nr_threads_arrived != barrier->max_threads) {
+        pthread_cond_wait(&barrier->cond, &barrier->mutex);
+    } else {
+        //Reseteamos preparando las variables para la siguiente invocación.
+        barrier->nr_threads_arrived = 0;
+        pthread_cond_broadcast(&barrier->cond);
+    }
+    
+    //Liberamos el cerrojo
+    pthread_mutext_unlock(&barrier->mutex);
+    
 	return 0;
 }
 
